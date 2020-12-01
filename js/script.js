6118eab6740f35995f0fb784022cfadc860c5561
kotlyarDG <kotlyar.d.g@gmail.com>
2020-12-01 16:59:42 +0300

    first

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 
 	$('.header-radar__burger').click(function (event) {
 		$('.header-radar__burger,.header-radar__menu').toggleClass('_active')
-		$('body').toggleClass('_lock')
 	});
 
 });
