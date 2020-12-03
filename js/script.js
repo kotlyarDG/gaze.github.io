70bb64873c9ac92964424b692957ed44827c04a4
kotlyarDG <kotlyar.d.g@gmail.com>
2020-12-03 12:29:08 +0300

    menu

--- js/script.js
+++ js/script.js
@@ -21,4 +21,47 @@ $(document).ready(function () {
 		$('.menu-cases__item').not($(this)).removeClass('_active');
 	});
 
+	var sections = $('.anchor-section');
+	var navItem = $('.menu__item');
+	var navHeight = $('.menu').outerHeight();
+	var nav = $('.menu');
+
+	$(document).on('click', '.menu__item', function (e) {
+		e.preventDefault();
+		if ($(window).width() < 991) {
+			$('.header-radar__burger,.header-radar__menu').toggleClass('_active');
+		}
+		var linkID = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(linkID).offset().top
+		}, 'slow');
+	});
+
+
+	if ($(window).width() >= 991) {
+		$(window).on('scroll', function () {
+			var curPos = $(this).scrollTop();
+
+			if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+				navItem.removeClass('_active');
+				sections.removeClass('_active');
+				$('#footer-radar').addClass('_active');
+				nav.find('a[href="#footer-radar"]').addClass('_active');
+			}
+
+			sections.each(function () {
+				var top = $(this).offset().top - navHeight;
+				var bottom = top + $(this).outerHeight();
+
+				if (curPos >= top && curPos <= bottom) {
+					navItem.removeClass('_active');
+					sections.removeClass('_active');
+
+					$(this).addClass('_active');
+					nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('_active');
+				}
+			});
+		});
+	}
+
 });
