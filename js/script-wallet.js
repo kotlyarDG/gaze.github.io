c9d491ef8a2dbd9752c50db67907769c243168db
kotlyarDG <kotlyar.d.g@gmail.com>
2020-12-04 13:56:54 +0300

    cards

---
+++ js/script-wallet.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+	var menuCard = $('.cards-wallet__menu-item');
+	var itemsCard = $('.item-card');
+	var idItem = $('.cards-wallet__menu-item._active').attr('data-index');
+
+	$('.item-card').fadeOut(400);
+	setTimeout(function () { $(`#item-card-${idItem}`).fadeIn(400); }, 400);
+
+
+
+	menuCard.click(function () {
+		itemsCard.fadeOut(400);
+		idItem = $(this).attr('data-index');
+		setTimeout(function () { $(`#item-card-${idItem}`).fadeIn(400); }, 400);
+		$(this).addClass('_active');
+		menuCard.not($(this)).removeClass('_active');
+	});
+
+});
