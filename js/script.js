f480a1c6900465931f5761288f7df40ceb005a9f
kotlyarDG <kotlyar.d.g@gmail.com>
2020-12-11 12:35:32 +0300

    cases content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // Проверка, загрузилась ли страница
 $(document).ready(function () {
 
+	function ibg() {
+
+		$.each($('.ibg'), function (index, val) {
+
+			if ($(this).find('img').length > 0) {
+				$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+			}
+
+		});
+	}
+
+	ibg();
+
 	$('.header-radar__burger').click(function (event) {
 		$('.header-radar__burger,.header-radar__menu').toggleClass('_active')
 	});
